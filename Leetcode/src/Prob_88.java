a54b5946a45a6166c3cdf3a89d2a9db2e6220632
Anush Saxena <saxenaanush88@gmail.com>
2023-07-02 21:19:54 +0530

    updated

--- Leetcode/src/Prob_88.java
+++ Leetcode/src/Prob_88.java
@@ -1,5 +1,20 @@
+// More optimized solution
 public class Prob_88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=m-1; int j=n-1;
+        while(i>=-1 & j>-1){
+            if (i>-1 && nums1[i] >= nums2[j]){
+                nums1[i+j+1] = nums1[i];
+                i-=1;
+            }
+            else{
+                nums1[i+j+1] = nums2[j];
+                j-=1;
+            }
+        }
+    }
 }
+
 // Brute Force approach.
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
