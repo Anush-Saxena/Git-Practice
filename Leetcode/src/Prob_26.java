be13a4e1b84fffe69d0ec1da22a1e35d3c40edde
Anush Saxena <saxenaanush88@gmail.com>
2023-07-05 23:53:03 +0530

    26 Uploaded

--- Leetcode/src/Prob_26.java
+++ Leetcode/src/Prob_26.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Prob_26 {
+public class Prob_26 {
+    //Basic solution (Brute force)
+    public int removeDuplicates(int[] nums) {
+        int count = 0;
+        for (int i=1; i<nums.length; i++){
+            if (nums[i]==nums[i-1]){
+                count+=1;
+                nums[i-1]=101;
+            }
+        }
+        Arrays.sort(nums);
+        return nums.length-count;
+    }
 }
