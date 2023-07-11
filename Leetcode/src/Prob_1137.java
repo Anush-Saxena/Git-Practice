c3c8fefacfc5ab6f6986e70d9af72a96e7f70050
Anush Saxena <saxenaanush88@gmail.com>
2023-07-11 18:32:18 +0530

    1137 Uploaded

---
+++ Leetcode/src/Prob_1137.java
@@ -0,0 +1,20 @@
+public class Prob_1137 {
+    // Iterative Approach
+    public int tribonacci(int n) {
+        if (n==0)
+            return 0;
+        else if (n==1 || n==2)
+            return 1;
+        else{
+            int a=0, b=1, c=1, d=0;
+            for (int i=3; i<=n; i++){
+                d =a+b+c;
+                a=b;
+                b=c;
+                c=d;
+            }
+            return c;
+        }
+    }
+    // In this case don't use recursive method it is very much time consuming.
+}
